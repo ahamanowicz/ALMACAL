0ac926059bebdf74aabb09ee967b701750e72422
AHamanow <ahamanowicz@gmail.com>
2018-07-02 17:01:19 +0200

    sensitivity function

--- co_volume.py
+++ co_volume.py
@@ -40,11 +40,11 @@ def CO_Lum_prime2(z,fCO, Sline, dv=200):
 	f_obs = fCO/(1+z)
 	Lline_prime = 3.25e7 * Sline * dv *  dL**2/(1+z)**3 / f_obs**2
 	return Lline_prime
-def gaussian(x, mu, sig):
-    return np.exp(-np.power(x - mu, 2.) / (2 * np.power(sig, 2.)))
 
-def sensitivity_function(r, Rmax):
-	I = np.cos(r/Rmax)
+def sensitivity_function(r, mu, sigma):
+
+	I = np.exp(-np.power(r - mu, 2.) / (2 * np.power(sigma, 2.)))
+
 	return I
 ######### Chose CO transition [GHz]
 # 0-CO(1-0) 1-CO (2-1) 2-CO(3-2) 3-CO(4-3) 4-CO(5-4) 5-CO(6-5) 6-CO(7-6)
@@ -53,6 +53,8 @@ CO = np.array([115.27,230.538, 354.796, 461.041, 576.268, 691.473, 806.652])
 
 # transition index 
 i = 2 # CO(3-2)
+
+
 
 ####### read data from the cube *stats file
 #single file (later change to whole sample or write short bash script)
@@ -65,13 +67,17 @@ rms =  float(content[1].split()[1])
 f1,f2 = float(content[7].split()[2]), float(content[7].split()[3]) #GHz
 chan_width = float(content[8].split()[2]) #GHz
 beam_radius = float(content[5].split()[3])*np.sqrt(2)*float(content[2].split()[4]) # imsize [pix] * sqrt(2) * cell [''/pix]
+imsize_pix = float(content[5].split()[3])
+cell = float(content[2].split()[4])
 
-
+imsize_arcsec = imsize_pix*cell # arces
+imsize_radians = np.radians(imsize_arcsec/3600.) #to go into the cosmology equation this must be in radians
 if (f1 > f2) : f1,f2 = f2,f1 # order asending
 f0 = abs(f1+f2)/2. # central frequency of the cube
 
-FOV  = fov(f0) # freq in GHz FOV of the detector ["]
-
+sigma = imsize_arcsec/2.35/1.5 # arcsec
+r_max = 1.7*sigma #arcsec
+print imsize_radians,sigma
 Sline = 5 *rms # line strength
 dv = 200 # [km/s] width of the line 
 
@@ -93,17 +99,33 @@ N = np.size(Z)
 nz = 100.
 dd = (z2-z1)/nz
 Vol_max=0
-for z in np.linspace(z1,z2,nz):
+print r_max
+dr = r_max/10.
+
+z1=0.2
+z2=0.3
+rmax=30.
+for z in np.linspace(z1,z2-dd,nz):
+	dS=0	
 	d = cosmo.luminosity_distance(z).value
-	Rmax = (FOV * d/ (1.+z)**2 )
-	dS=0
-	for r in np.linspace(0,Rmax,10):
-		dS+=2*np.pi*Rmax/10. / sensitivity_function(r, Rmax) 
-	Vol_max += dS * dd #Mpc**3 physical
+	for r in np.linspace(0,r_max-dr,100): # r in arcsec
+		cr = 1#sensitivity_function(r,mu=0.,sigma=sigma)#correction on intensity function rin arcsec
+		#field of a ring in Mpc^2
+		R1 = (np.radians(r/3600.) * d/ (1.+z)**2 ) #Mpc r " -> radians
+		R2 = (np.radians((r+dr)/3600.) * d/ (1.+z)**2 ) #Mpc 
+		dS+=np.pi*(R2**2 - R1**2)*cr
+		#print sensitivity_function(r,mu=0.,sigma=sigma)
+	d1 = cosmo.luminosity_distance(z).value
+	d2 = cosmo.luminosity_distance(z+dd).value
+	Vol_max += dS * (d2-d1) #Mpc**3 physical
 Vol_com_max = Vol_max *(1+z2)**3# / 1.e9 #Gpc
 
-
-
+print z1,z2, Vol_max, Vol_com_max
+d1,d2 = cosmo.luminosity_distance(z1).value,  cosmo.luminosity_distance(z2).value
+#print fov(f0), imsize_arcsec 
+r1,r2 =  np.radians(rmax/3600.) * d1/ (1.+z1)**2 , np.radians(rmax/3600.) * d2/ (1.+z2)**2 
+print d1,r1,d2,r2
+"""
 if z2 > 0 : #proceed
 	if z1 < 0: z1 = 0 #if the transition lower limit dont fit the cube we start integrating at 0 
 	for n in range(N):
@@ -112,15 +134,20 @@ if z2 > 0 : #proceed
 		if Z[n] < z2 and Z[n] > z1:
 			dd = (Z[n]-z1)/nz
 			Vol = 0
-			for z in np.linspace(z1,Z[n],nz):
+			for z in np.linspace(z1,Z[n]-dd,nz):
+
 				d = cosmo.luminosity_distance(z).value
-				Rmax = (FOV * d/ (1.+z)**2 )
 				dS=0
-				for r in np.linspace(0,Rmax,10):
-					dS+=2*np.pi*Rmax/10. / sensitivity_function(r, Rmax)
-				#dS = np.pi*(FOV * d/ (1.+z)**2 )**2 
-				Vol += dS * dd #Mpc**3 physical
-			Vol_com = Vol *(1+Z[n])**3 #/ 1e9 #G
+				for r in np.linspace(0,r_max-dr,10): # r in arcsec
+					cr = sensitivity_function(r,mu=0.,sigma=sigma)#correction on intensity function rin arcsec
+					#field of a ring in Mpc^2
+					R1 = (np.radians(r/3600.) * d/ (1.+z)**2 ) #Mpc r " -> radians
+					R2 = (np.radians((r+dr)/3600.) * d/ (1.+z)**2 ) #Mpc 
+					dS+=np.pi*(R2**2 - R1**2)/ cr
+				d1 = cosmo.luminosity_distance(z).value
+				d2 = cosmo.luminosity_distance(z+dd).value
+				Vol += dS * (d2-d1) #Mpc**3 physical
+			Vol_com = Vol *(1+Z[n])**3 # Mpc***3
 			plt.plot(LCO[n], Vol_com,'ro')
 		if Z[n] > z2:
 			if LCO[n] < 1e8:
@@ -130,64 +157,6 @@ if z2 > 0 : #proceed
 else: 	
 	print "Transition outside the cube frequency range!"
 	Volume = 0	
-#ideal case
-plt.show()
-
-"""
- 
-
-	Vol = 0.
-	dL1, dL2 = cosmo.luminosity_distance(z1).value, cosmo.luminosity_distance(z2).value # [Mpc], limits of the integration 
-	nd = 100 # 100 elements for the integral
-	dist = np.linspace(dL1,dL2,nd) 
-	dd = abs(dL2-dL1)/nd #thinckes of the cone slice Mpc
-
-	#print z_at_value(cosmo.luminosity_distance, dL1*u.Mpc)
-	
-	#perfect detector case
-	for d in dist:  
-	
-		# volume element
-		z = z_at_value(cosmo.luminosity_distance, d*u.Mpc)
-		dS = np.pi*(FOV * d/ (1.+z)**2 )**2 
-		Vol += dS * dd #Mpc**3 physical
-		Vol_com = Vol *(1+z)**3 / 1e9 #Gpc
-		#col = CO_Lum(z,co,Sline)
-		#col_p = CO_Lum_prime(col.value, co)
-		col_p2 = CO_Lum_prime2(z,co, Sline, dv=200) # K km/s pc^2
-		#print np.log10(col_p), Vol_com/1e9
-		plt.plot(np.log10(col_p2),Vol_com, 'bo')
-		#print Vol/1e9, z, d
-		#\print Vol_com , np.log10(col_p2)
-plt.xlabel("L_CO' [K km/s pc^2]" )
-plt.ylabel('Volume comoving [Gpc^3]')
 plt.show()
 """
-"""
 
-
-
-
-####### signal redshift limit  ######
-n_sigma = 5 
-dv = 200 # signal width in km/s
-c = 3e5 # speed of ight in km/s
-
-
-#### integrate over observation cone - physical FOV changes with redshift
-Z = 2.0 # depth
-dz = 0.001
-distance = np.arange(0,Z,dz)
-Volume = 0.
-
-for i in range(np.size(distance)):
-	z = distance[i]
-	d =  cosmo.luminosity_distance(z) #Mpc
-
-	dS = np.pi*(FOV * d/ (1.+z)**2 )**2 #FOV in radians, dL in Gpc
-	d_dist = cosmo.luminosity_distance(z+dz) - cosmo.luminosity_distance(z)
-	Volume += dS * d_dist #Gpc**3 physical
-vol = Volume.value/1.e9	
-vol_com = vol * (1+Z)**3 #Gpc**3 comoving
-print vol,"Gpc^3",vol_com, "Gpc^3"
-"""
